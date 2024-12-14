f799af10901c3ad23e92e1906f777e465ba0ac0d
SSPRADHAN <sspradhan@SSPRADHANs-MacBook-Pro.local>
2024-12-14 19:12:59 +0530

    EBS-Instance

--- EBS Volume/createinstance.tf
+++ EBS Volume/createinstance.tf
@@ -1,9 +1,30 @@
+Provide "aws" {
+	region  = "ap-south-1"
+}
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
-  subnet_id = aws_subnet.levelupvpc-public-2.id
+  
   tags = {
-    Name = "terrafom-elb"
+    Name = "custom-instance"
   }
+}
+
+#EBS resource Creation
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "ab-south-1a"
+  size              = 15
+  type              = "gp2"
+
+  tags = {
+    Name = "Secondary Volume Disk"
+  }
+}
+
+#Atatch EBS volume with AWS Instance
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id   = aws_ebs_volume.ebs-volume-1.id
+  instance_id = aws_instance.MyFirstInstnace.id
 }
