76fa315034c991505dedafe5e566ba98ec48aa6b
SSPRADHAN <sspradhan@SSPRADHANs-MacBook-Pro.local>
2024-12-14 13:08:51 +0530

    VPC_Instance

--- VPC_NAT_Gatway_EC2/createinstance.tf
+++ VPC_NAT_Gatway_EC2/createinstance.tf
@@ -1,27 +1,13 @@
-provider "aws" {
-	region  = "ap-south-1"
-}
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
+  subnet_id = aws_subnet.levelupvpc-public-2.id
 
   tags = {
     Name = "custom_instance"
   }
 
-
-  
-
-  provisioner "file" {
-      source = "installNginx.sh"
-      destination = "/tmp/installNginx.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/installNginx.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
-      "sudo /tmp/installNginx.sh",
-    ]
-  }
 }
