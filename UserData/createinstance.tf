b6aec3e46e43e5da83fedd5ac34b9e9d7257a4f0
SSPRADHAN <sspradhan@SSPRADHANs-MacBook-Pro.local>
2024-12-14 20:01:33 +0530

    Userdata

---
+++ UserData/createinstance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+	region  = "ap-south-1"
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-053b12d3152c0cc71"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1b" 
+  key_name  = "My-Key-MuM-2425"
+  user_data = file.(installapache.sh)
+  tags = {
+    Name = "Userdata"
+  }
+}
+
