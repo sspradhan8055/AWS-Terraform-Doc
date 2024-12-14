a782e2928610b0259849d34333c7042e8a1f6269
SSPRADHAN <sspradhan@SSPRADHANs-MacBook-Pro.local>
2024-12-14 13:41:39 +0530

    VPC_Instance

--- VPC_NAT_Gatway_EC2/creationInstance.tf
+++ VPC_NAT_Gatway_EC2/creationInstance.tf
@@ -1,13 +1,10 @@
-
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-053b12d3152c0cc71"
   instance_type = "t2.micro"
-
   vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
   subnet_id = aws_subnet.levelupvpc-public-2.id
-
   tags = {
-    Name = "custom_instance"
+    Name = "terrafom-elb"
   }
-
 }
+
